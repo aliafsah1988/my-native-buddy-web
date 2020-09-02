1e75dee5650cf2bd8b242cab2d641d2ac95cfb44
Ali Afsahnoudeh <ali.afsah1988@gmail.com>
2020-09-02 18:23:57 +0430

    defaul lang added

--- src/services/pronunciation.service.js
+++ src/services/pronunciation.service.js
@@ -1,9 +1,11 @@
 import LangService from "./lang.service";
+import config from "../config";
 
 class PronunciationService {
   async play(word) {
     if (!word) return;
     let msg = new SpeechSynthesisUtterance(word.text);
+    if (!word.langId) word.langId = config.DEFAULT_LANG_ID;
     const lang = await LangService.getLangById(word.langId);
     msg.lang = lang.code;
     window.speechSynthesis.speak(msg);
