522b79f77970bc16b45fc6cd3e7b356958b98f31
Ali Afsahnoudeh <aliafsahnoudeh@gmail.com>
2022-05-17 15:16:20 +0200

    add LoginView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="login-container">
+      <div class="login-container__box">
+        <div class="login-container__box__item">
+          <label for="name">Username</label>
+          <input v-model="email" />
+        </div>
+        <div class="login-container__box__item">
+          <label for="name">Password</label>
+          <input v-model="password" />
+        </div>
+        <div class="login">
+          <button @click="login">Login</button>
+        </div>
+        <span>
+          If you don't have an account please
+          <!-- <router-link :to="{ name: 'Register' }">Register</router-link> -->
+          first.
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import AuthService from "../../_old/src/services/auth.service";
+import { useRouter } from "vue-router";
+
+const email = ref<string>("");
+const password = ref<string>("");
+const router = useRouter();
+
+const login = async () => {
+  const result = await AuthService.login(email.value, password.value);
+  if (result && result.length > 0) {
+    router.push({ name: "PracticeGroup" });
+  }
+};
+</script>
