5f33c2b52a2bae1b2e76464f623130274f3829c8
ArezooGoshtasbi <arezoo.goshtasbi69@gmail.com>
2022-05-27 16:55:21 +0200

    add field component

---
+++ src/components/Form/Field/Field.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="g-form-field">
+    <Label v-if="label">{{ label }}</Label>
+    <div class="g-form-field__content">
+      <slot :error="error" />
+    </div>
+    <span v-if="error && error.length > 0" class="g-form-error">{{
+      error
+    }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import Label from "../Label/Label.vue";
+import "./style.scss";
+
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  error: {
+    type: String,
+    default: "",
+  },
+});
+</script>
---
+++ src/components/Form/Label/Label.vue
@@ -0,0 +1,14 @@
+<template>
+  <label class="g-form-label">
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+// Style
+import "./style.styl";
+
+export default {
+  name: "Label"
+};
+</script>
