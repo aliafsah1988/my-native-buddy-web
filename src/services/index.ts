fb819d6804e68e88299985efb2a544edff47b50b
Ali Afsahnoudeh <aliafsahnoudeh@gmail.com>
2022-09-12 18:01:14 +0200

    env usage in service injection

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,7 +4,7 @@ import WordService from "./WordService";
 import LangService from "./LangService";
 import AuthService from "./AuthService";
 
-const apiService = new ApiService("/-");
+const apiService = new ApiService(import.meta.env.VITE_SERVER_BASE_URL);
 const groupService = new GroupService(apiService);
 const wordService = new WordService(apiService);
 const langService = new LangService(apiService);
